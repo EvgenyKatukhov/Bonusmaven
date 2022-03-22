310f76b79d77072e5c8dd65c8cca1084c4c01c3a
EvgenyKatukhov <ekatuhov@gmail.com>
2022-03-22 09:12:35 +0300

    new 1

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -33,6 +33,7 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
+
     @org.junit.jupiter.api.Test
     void shouldCalculateForNoRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
@@ -48,6 +49,7 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
+
     @org.junit.jupiter.api.Test
     void shouldCalculateForNoRegisteredAndOverLimit() {
         BonusService service = new BonusService();
